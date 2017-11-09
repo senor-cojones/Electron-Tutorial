48105c37481d839d86dcdda23d6ae01017347b90
Aaron White <white_aaron@hotmail.co.uk>
2017-11-09 23:21:21 +0000

    3- Add screenshot capture and save

---
+++ 3-Screenshot/app/renderer.js
@@ -0,0 +1,37 @@
+const electron = require("electron");
+const fileSystem = require("fs");
+const path = require("path");
+const desktopCapturer = electron.desktopCapturer;
+const ipc = electron.ipcRenderer;
+const window = electron.window;
+
+function onCapture(event, targetDir) {
+    getImageSource(desktopCapturer, window, source => {
+        const png = source.thumbnail.toPng();
+        const filePath = path.join(targetDir, new Date() + ".png");
+
+        createScreenshot(png, filePath);
+    });
+}
+
+function getImageSource(desktopCapturer, window, done) {
+    const options = {
+        types: ["window"]
+    };
+
+    desktopCapturer.getSources(options, (error, sources) => {
+        if (error) return console.log("Cannot capture window:", error);
+
+        const isImageSource = source => source.name === "Electron Skeleton App";
+
+        done(sources.filter(isImageSource)[0]);
+    });
+}
+
+function createScreenshot(png, filePath) {
+    fileSystem.writeFile(filePath, png, error => {
+        if (error) return console.log("Failed to save screenshot:", error);
+    });
+}
+
+ipc.on("capture", onCapture);
